d759ff003283decd09c2cc350473ab0a17f3b9a3
Lorraine-Olegario <lorraineolegario16@gmail.com>
2025-07-14 16:49:09 -0700

    test: refactor api externa

--- tests/Feature/ApiExternalProcuctsTest.php
+++ tests/Feature/ApiExternalProcuctsTest.php
@@ -18,8 +18,34 @@ class ApiExternalProcuctsTest extends TestCase
         $this->pathAPI = 'https://fakestoreapi.com';
     }
 
-    public function test_api_products_status(): void
+    /*public function test_api_products_status(): void
     {
+        $response = Http::get($this->pathAPI .  '/products');
+        $this->assertEquals(200, $response->status());
+    }*/
+
+    /*public function test_import_products_from_api(): void
+    {
+        Queue::fake();
+
+        $response = Http::get($this->pathAPI .  '/products');
+        $this->assertEquals(200, $response->status());
+
+        $this->artisan('products:import')
+             ->expectsOutput('Importando produtos...')
+             ->assertExitCode(0);
+
+        Queue::assertPushed(ImportProductsFromApi::class);
+    }*/
+
+        public function test_api_products_status(): void
+    {
+        Http::fake([
+            $this->pathAPI . '/products' => Http::response([
+                ['id' => 1, 'title' => 'Test Product', 'price' => 10.99],
+            ], 200),
+        ]);
+
         $response = Http::withHeaders([
             'User-Agent' => 'LaravelTestBot/1.0',
             'Accept' => 'application/json',
@@ -31,6 +57,12 @@ class ApiExternalProcuctsTest extends TestCase
     public function test_import_products_from_api(): void
     {
         Queue::fake();
+
+        Http::fake([
+            $this->pathAPI . '/products' => Http::response([
+                ['id' => 1, 'title' => 'Test Product', 'price' => 10.99],
+            ], 200),
+        ]);
 
         $response = Http::withHeaders([
             'User-Agent' => 'LaravelTestBot/1.0',
